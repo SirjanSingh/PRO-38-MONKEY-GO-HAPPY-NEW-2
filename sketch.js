ffdaa3f88053534907c5a8e50df7ddea252a4683
SirjanSingh <sirjan.singh036@gmail.com>
2020-08-18 21:52:11 +0530

    used camera

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@
 var monkeyImages,monkey;
 var stoneImage,stone;
 var bananaImage,banana;
-var jungle,jungleImage;
+var jungle,jungleI;
 var inviGround;
 var score=0;
 var PLAY=1;
@@ -10,6 +10,7 @@ var END=0;
 var gameState=PLAY;
 var life=3;
 var  gameOverImage,restartImage,gameOver,restart;
+
 function preload(){
   monkeyImages=loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png");
   
@@ -17,25 +18,32 @@ function preload(){
  
 stoneImage=loadImage("stone.png");
   
-  jungleImage=loadImage("jungle.jpg");
-  gameOverImage=loadImage("gameOver.png");
+jungleI=loadImage("jungle.jpg");
+
+gameOverImage=loadImage("gameOver.png");
   restartImage=loadImage("restart.png")                                   
 }                                                                                      
 function setup() {
   createCanvas(600,300);
   
-  jungle=createSprite(60,0,200,100);
-  jungle.addImage("jungle",jungleImage);
-  jungle.scale=1.5;
-  jungle.x=jungle.width/2
-
-  jungle.velocityX=-5;
+ // jungle=createSprite(880,0,200,100);
+ // jungle.width = 1000
+  //jungle.addImage("jungle",jungleI);
+  //jungle.scale=1;
+/*
+  jungle1=createSprite(290,150,200,100);
+  jungle1.addImage("jungle",jungleI);
+  jungle1.scale=0.6;
+*/
+  //jungle.velocityX=-5;
   monkey=createSprite(100,250,20,10);
   monkey.addAnimation("running",monkeyImages);
   monkey.scale=0.08;
+  monkey.velocityX = 6
   
   inviGround=createSprite(280,300,600,80);
   inviGround.visible=false;
+  inviGround.velocityX = 6;
   
   gameOver=createSprite(300,150);
   gameOver.addImage("gameOver",gameOverImage);
@@ -47,19 +55,42 @@ function setup() {
   
   stones=new Group();
   food=new Group();
+  //monkey.debug = true
+  //console.log(jungle.width , jungle.height)
 }
 
 
 function draw(){
- background(255);
+ background(jungleI);
   monkey.collide(inviGround);
+  
+  //console.log(camera.position.x%912<6,jungle1.x)
+  //console.log(camera.position.x%612===0)
   if(gameState===PLAY){
     monkey.visible=true;
-            if(jungle.x<0){
-            jungle.x=jungle.width/2
-           }
-            jungle.velocityX=-6-score/15  ;
+    camera.position.x = monkey.x+200;
+  //  camera.position.y = height/2;
+            
+             //jungle.velocityX=-6-score/15  ;
+var a= 612
 
+/*
+if(camera.position.x%1224===0){
+  //jungle1.x=1484;
+  jungle.x+=1194
+  console.log(jungle.x)
+
+  }
+  
+if(camera.position.x%a===0){
+//jungle1.x=1484;
+jungle1.x+=1194
+console.log(jungle1.x,jungle.x)
+}
+*/
+
+ // jungle.x = camera.position.x
+ 
 
 
           if (keyDown("space")&&monkey.y>208){
@@ -112,7 +143,7 @@ function draw(){
     restart.visible=true;
     stones.destroyEach();
     food.destroyEach();
-    jungle.velocityX=0;
+   // jungle.velocityX=0;
     monkey.visible=false;
     if(mousePressedOver(restart)){
        reset();
@@ -121,11 +152,15 @@ function draw(){
  
  
   drawSprites();
+
+ a=camera.position.x +30;
+ // var   b=(-monkey.y + camera.position.y) + 1    20;
+// console.log(b)
   stroke("white");
   textSize(20);
   textFont("gothic");
-  text("YOUR SCORE IS "+score,380,20);
-    text("LIVES LEFT: "+life,80,20);
+  text("YOUR SCORE IS "+score,a,20);
+    text("LIVES LEFT: "+life,a-300 ,20);
 }
 function reset(){
   gameState=PLAY;
@@ -138,23 +173,23 @@ monkey.x=100;
 monkey.y=250;
 }
 function createBananas(){
-   if(frameCount%80===0){
-        banana=createSprite(600,random(120,200),30,10);
+   if(camera.position.x%480===0){
+        banana=createSprite(camera.position.x+300,random(120,200),30,10);
         banana.addImage("static",bananaImage);
         banana.scale=0.04;
-        banana.velocityX=-(10+score/15);
-       banana.lifetime=60;
+       banana.velocityX=-(1+score/15);
+      // banana.lifetime=60;
 
      food.add(banana);
   }
 }
 function createStones(){
-if(frameCount%300===0){
-  stone=createSprite(600,250,30,10);
+if(camera.position.x%2400===0){
+  stone=createSprite(camera.position.x+300,250,30,10);
   stone.addImage("static",stoneImage);
   stone.scale=0.1;
-  stone.velocityX=-(10+score/15);
-  stone.lifetime=60;
+  stone.velocityX=-score/20;
+  //stone.lifetime=60;
 stones.add(stone);
 }
 }
